80cb5388870ea0f2565536976a57cd4e504bce21
Loki-Melapattu <lokeswarimelapattu1707@gmail.com>
2025-04-20 13:49:52 +0300

    Part2- Phonebook exercise completed

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -3,9 +3,21 @@ import Persons from './components/Persons.jsx'
 import Filter from './components/Filter.jsx'
 import PersonAdd from './components/PersonAdd.jsx'
 import phoneService from './services/phonebook'
-import axios from 'axios'
+import './index.css'
 
-const apiUrl = 'http://localhost:3001/persons' // Replace with your actual API URL
+const Notification = ({ isError, message }) =>
+{
+  if (message === null)
+  {
+    return null
+  }
+  return (
+    <div className={isError ? 'error' : 'success'}>
+      {message}
+    </div>
+  )
+}
+
 const App = () =>
 {
 
@@ -13,7 +25,21 @@ const App = () =>
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [isError, setIsError] = useState(false)
 
+
+  // Handle error message
+  const handleNotification = (isError, message) =>
+  {
+    setIsError(isError)
+    setErrorMessage(message)
+    setTimeout(() =>
+    {
+      setErrorMessage(null)
+    }
+      , 5000)
+  }
   // Handle name change
   const handleNameChange = (event) =>
   {
@@ -36,23 +62,23 @@ const App = () =>
   const handleSubmit = (event) =>
   {
     event.preventDefault()
-    if (persons.some(person => person.name === newName))
+    const existingPerson = persons.find(person => person.name === newName)
+    if (existingPerson)
     {
       if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`))
       {
-        const existingPerson = persons.find(person => person.name === newName)
         const updatedPerson = { ...existingPerson, number: newNumber }
         phoneService
           .update(existingPerson.id, updatedPerson)
           .then(data =>
           {
-            console.log('Updated:', data)
+            handleNotification(false, `Number updated to the person ${newName}`)
             setPersons(persons.map(person => person.id !== existingPerson.id ? person : updatedPerson))
           })
           .catch(error =>
           {
+            handleNotification(true, `Error while updating the person ${newName}`)
             console.log('Error while updating person:', error)
-            alert('Error while updating person')
           });
       }
     } else
@@ -62,16 +88,17 @@ const App = () =>
         .create(newPerson)
         .then(response => 
         {
+          handleNotification(false, `Added ${newName}`)
           console.log('Added:', response)
           setPersons(persons.concat(newPerson))
         })
         .catch(error =>
         {
+          handleNotification(true, `Information of ${newName} has already been removed from server`)
           console.log('Error while adding person:', error)
           alert('Error while adding person')
         });
     }
-
     setNewName('')
     setNewNumber('')
   }
@@ -79,15 +106,20 @@ const App = () =>
   // Handle delete
   const handleDelete = (id) =>
   {
-    if (window.confirm(`Delete ${persons.find(person => person.id === id).name} ?`))
+    const delPerson = persons.find(person => person.id === id)
+    if (window.confirm(`Delete ${delPerson.name}?`))
     {
       phoneService
         .remove(id)
-        .then(() => setPersons(persons.filter(person => person.id !== id)))
+        .then(() =>
+        {
+          setPersons(persons.filter(person => person.id !== id))
+          handleNotification(false, `Deleted ${delPerson.name}`)
+        })
         .catch(error =>
         {
+          handleNotification(true, `Information of ${delPerson.name} has already been removed from server`)
           console.log('Error while deleting person:', error)
-          alert('Error while deleting person')
         });
     }
   }
@@ -96,7 +128,6 @@ const App = () =>
   const fetchPersons = () =>
   {
     console.log('Fetching persons from server...')
-
     phoneService
       .getAll()
       .then(response =>
@@ -107,6 +138,7 @@ const App = () =>
       })
       .catch(error =>
       {
+        handleNotification(true, 'Error fetching persons')
         console.error('Error fetching persons:', error)
       })
   }
@@ -116,9 +148,10 @@ const App = () =>
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification isError={isError} message={errorMessage} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
-      <h2>Add a new</h2>
+      <h2>Add a new person</h2>
       <PersonAdd
         newName={newName}
         newNumber={newNumber}
