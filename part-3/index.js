06b69116c5721644270ed420247a2a00312b6bfe
Loki-Melapattu <lokeswarimelapattu1707@gmail.com>
2025-07-14 14:38:23 +0300

    uibuild

---
+++ part-3/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+const app = express();
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+app.use(cors());
+app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+ 
+
+app.get('/info', (request, response) => {
+  response.send('<h1>Hello Worl2333!</h1>');
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    } 
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body;
+    if (!person || !person.name) {
+        return response.status(400).json({ error: 'Name missing' });
+    }
+    if (!person.number) {
+        return response.status(400).json({ error: 'Number missing' });
+    }
+    // Check if the name already exists
+    if (persons.some(p => p.name === person.name)) {
+        return response.status(400).json({ error: 'Name must be unique' });
+    }
+     // Generate a new random ID in a large range
+  const id = Math.floor(Math.random() * 1000000)
+    // Check if the ID already exists
+    if (persons.some(p => p.id === id.toString())) {  
+        return response.status(400).json({ error: 'ID already exists' });
+    }
+    // Add the new person with the generated ID
+    person.id = id; 
+    persons = persons.concat(person);
+    response.json(person);
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});  
